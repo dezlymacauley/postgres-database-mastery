01c71cb17f96d925f95e635382357f21de2b89dc
dezlymacauley <dezlymacauley@proton.me>
2026-01-31 12:57:43 +0200

    Added guide on boolean type

---
+++ 02-column-types/03_boolean.sql
@@ -0,0 +1,61 @@
+-- ABOUT: Boolean
+
+-- The boolean type in PostgreSQL is one byte.
+-- _______________________________________________________________________________
+
+-- Internally, PostgreSQL stores boolean values in this format:
+-- 0 = False
+-- 1 = True
+
+-- _______________________________________________________________________________
+
+CREATE TABLE player_status (
+    user_name TEXT,
+    is_online BOOLEAN 
+);
+
+-- _______________________________________________________________________________
+
+-- When it comes to entering a value in a boolean column,
+-- Postgres will accept:
+--
+-- TRUE, FALSE
+-- 't', 'f'
+-- 'yes', 'no'
+-- '1', '0'
+--
+-- Also Note: Boolean input in PostgreSQL is case-insensitive.
+
+INSERT INTO player_status (
+    user_name, is_online
+) 
+VALUES
+    ('Dave', true),
+    ('Ken', false),
+    ('Lee', '1'),
+    ('Jin', '0'),
+    ('Max', 't'),
+    ('Jane', 'f'),
+    ('Gojo', 'yes'),
+    ('Bruce', 'no');
+
+-- _______________________________________________________________________________
+
+SELECT * FROM player_status;
+
+-- These will be the output
+
+-- +-----------+-----------+
+-- | user_name | is_online |
+-- |-----------+-----------|
+-- | Dave      | True      |
+-- | Ken       | False     |
+-- | Lee       | True      |
+-- | Jin       | False     |
+-- | Max       | True      |
+-- | Jane      | False     |
+-- | Gojo      | True      |
+-- | Bruce     | False     |
+-- +-----------+-----------+
+
+-- _______________________________________________________________________________
